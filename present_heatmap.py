1648e3fca32201ddb9b86bacf598a5ba963289b1
MinewolfST <69927765+MinewolfST@users.noreply.github.com>
2023-12-02 15:11:05 +0100

    Add files via upload

---
+++ present_heatmap.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the CSV file
+df = pd.read_csv('output_field_vel.csv', header=None) 
+
+# Convert data from csv to NumPy array
+data = df.to_numpy() 
+
+ # Create a heatmap
+plt.imshow(data, cmap='viridis', interpolation='nearest') 
+plt.colorbar()  
+plt.show()
